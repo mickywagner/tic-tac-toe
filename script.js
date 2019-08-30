c86911ed29f5f07dc6e2181960fb89fbb07d5f6b
Michaela Wagner <mickywagner@gmail.com>
2019-08-29 20:27:56 -0600

    Add gameboard object + array and display in table

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const playGamer = function() {
-    console.log('get ready to build tic tac toe')
-}
+let X = 'X'
+let O = 'O'
 
-playGamer()
+let Gameboard = {
+    gameboard: [
+        [X, X, O],
+        [O, O, X],
+        [X, X, O]
+    ]
+};
+
+const player = name => {
+    name
+    return { name }
+};
+
+let displayGame = () => {
+    let table = document.querySelector('table')
+        for(let i = 0; i<table.rows.length; i++) {
+            for(let j = 0; j < table.rows[i].cells.length; j++) {
+                table.rows[i].cells[j].innerHTML = Gameboard.gameboard[i][j]
+            }   
+        }
+}
