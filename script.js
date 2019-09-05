8d56469f613ca152c41f066fe47a0efebb8ea046
Michaela Wagner <mickywagner@gmail.com>
2019-09-04 21:39:36 -0600

    Adjust gameboard and display function to regular array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let Gameboard = {
     gameboard: [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
+        "", "", "", 
+        "", "", "", 
+        "", "", ""
     ]
 };
 
@@ -21,15 +21,16 @@ let GameObj = {
     otherPlayer: p2
 };
 
-let displayGame = () => {
-    let table = document.querySelector('table')
-        for(let i = 0; i<table.rows.length; i++) {
-            for(let j = 0; j < table.rows[i].cells.length; j++) {
-                table.rows[i].cells[j].innerHTML = Gameboard.gameboard[i][j]
-                table.rows[i].cells[j].dataset.key = i + "," + j
-            }   
+function displayGameboard() {
+    let cell = document.querySelectorAll('td')
+    cell.forEach((cell) => {
+        for(i = 0; i < Gameboard.gameboard.length; i++){
+            Gameboard.gameboard[i] = cell.innerHTML 
         }
-};
+    })
+}
+
+// needs to go inside something - Game ?
 
 let table = document.querySelector('table')
 table.addEventListener('click', function(e) {
@@ -45,12 +46,53 @@ table.addEventListener('click', function(e) {
     }
 })
 
+function resetGame() {
+    Gameboard.gameboard = [
+        "", "", "", 
+        "", "", "", 
+        "", "", ""
+    ]
+}
+
+// // Check if values are equal
+// const allEqual = arr => arr.every(v => v === arr[0])
+
+// //  00 01 02 Gameboard.gameboard[0]
+// //  10 11 12 Gameboard.gameboard[1]
+// //  20 21 22 Gameboard.gameboard[2]
+
+// // Get columns array
+// const arrayColumn = (arr, n) => arr.map(x => x[n])
+
+let wins = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 
-// {
-//     let key = e.target.dataset.key
-//     if(Gameboard.gameboard[key[0]][key[2]] == "") {
-//         Gameboard.gameboard[key[0]][key[2]] = getMark()
-//     }})
+function isWinner(board) {
+    for(let i = 0; i < wins.length; i++) {
+        let a, b, c;
 
+        console.log(board[wins[i][0]])
+        console.log(board[wins[i][1]] )
+        console.log(board[wins[i][2]])
 
+    }
+
+    //     a = board[wins[i][0]] 
+    //     b = board[wins[i][1]] 
+    //     c = board[wins[i][2]] 
+
+    //     if(a == b && a == c && a != "") {
+    //         return a
+    //     }
+    // }
+    // return false
+}
